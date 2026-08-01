803d8338d79f7cd1094204202be2922824cfa97f
Shubhi240604 <shubhirai024@gmail.com>
2026-08-01 23:13:05 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (9.63%) - LeetSync

---
+++ 4365-count-valid-prefixes/count-valid-prefixes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int validCount = 0;
+        int count0 = 0;
+        int count1 = 0;
+        
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                count0++;
+            } else {
+                count1++;
+            }
+            
+            
+            if (Math.abs(count0 - count1) <= 1) {
+                validCount++;
+            }
+        }
+        
+        return validCount;
+    }
+}
